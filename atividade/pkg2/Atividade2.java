d3f0a9544f4ac65437cc586b7113a15c938b8c81
Giovannip11 <125603962+Giovannip11@users.noreply.github.com>
2024-03-13 20:00:22 -0300

    Update Atividade2.java

--- atividade/pkg2/Atividade2.java
+++ atividade/pkg2/Atividade2.java
@@ -6,11 +6,12 @@ public class Atividade2 {
 
     
     public static void main(String[] args) {
-         double pesoTotal=0 ;
-         double pesoMÁX= Double.MIN_VALUE;
-         double pesoMIN= Double.MAX_VALUE;
+        float pesoTotal=0 ;
+         Float pesoMÁX= Float.MIN_VALUE;
+         Float pesoMIN= Float.MAX_VALUE;
          int qtdBois = 0;
-         double peso;
+         float peso;
+         float media = pesoTotal / qtdBois;
        
       while(true){
           peso = InOut.leFloat("Digite o peso do boi(kg)");
@@ -30,9 +31,9 @@ public class Atividade2 {
       if(qtdBois == 0){
           InOut.MsgDeInformacao("Informação", "Nenhum boi foi registrado");
       }else{
-          double media = pesoTotal / qtdBois;
+          
           InOut.MsgDeInformacao("Resultado", "Peso médio do rebanho é: "+ media + "kg");
-          InOut.MsgDeInformacao("Resultado", "Peso do boi mais gorddo é: " + pesoMÁX + "kg");
+          InOut.MsgDeInformacao("Resultado", "Peso do boi mais gordo é: " + pesoMÁX + "kg");
           InOut.MsgDeInformacao("Resultado", "Peso do boi mais leve é: " + pesoMIN + "kg");
       }
         
