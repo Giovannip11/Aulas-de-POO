387189ffd63be1a0849d693d00eed11a6876ff26
Giovannip11 <125603962+Giovannip11@users.noreply.github.com>
2024-03-20 20:44:39 -0300

    Add files via upload

---
+++ Exercicio sla lek/src/Anúncio.java
@@ -0,0 +1,48 @@
+
+
+
+public class Anúncio {
+    public static void main(String[] args) {
+        
+    
+    int id;
+    String palavras;
+    String diasPublicacao;
+    float valor;
+    String Titulo;
+    String dataInsercao;
+    int publicar;
+    String nomeContato;
+    String telefone;
+    String observacao;
+    
+    
+ 
+   
+    
+    
+}
+
+    public Anúncio() {
+    }
+
+     public void remeterEmail(){
+        System.out.println("Remetendo email...");
+    }
+     public void manter(){
+         System.out.println("Mantendo anuncio...");
+     }
+     public void anunciar(){
+         System.out.println("Anunciando...");
+     }
+     public String locarlizar(){
+         System.out.println("Localizando...");
+        return null;
+     }
+    
+    
+     
+    
+   
+    
+}
