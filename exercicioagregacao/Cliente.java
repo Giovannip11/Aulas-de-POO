0714990c0e6e54881508ed4b954b772d66532d8e
Giovannip11 <125603962+Giovannip11@users.noreply.github.com>
2024-04-01 21:30:39 -0300

    Update Cliente.java

--- exercicioagregacao/Cliente.java
+++ exercicioagregacao/Cliente.java
@@ -7,5 +7,12 @@ public class Cliente {
     public String nome;
     public String endereco;
     public String cpf;
+
+    Cliente(){
+    this.nome = nome;
+    this.endereco = endereco;
+    this.cpf = cpf;
+    }
+    
     
 }
